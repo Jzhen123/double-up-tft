e43b262c56c8aff8ab9cebe4cceda36cef172ab9
Jzhen123 <jiayuzheng01@gmail.com>
2022-01-13 20:01:18 -0500

    Setup node and react server/files.

---
+++ client/src/components/homepage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Homepage() {
+ 
+
+  return (
+    <div>Hi</div>
+  );
+}
+
+export default Homepage;
---
+++ client/src/components/login.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Login() {
+ 
+
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default Login;
