029b000c194825d79c17246dcf00dea15bd6e73c
Jzhen123 <jiayuzheng01@gmail.com>
2022-01-18 14:11:39 -0500

    Removed isUserSignedIn logic that came from initial setup.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,39 @@
 import React, { useState } from "react";
 import { TabMenu } from 'primereact/tabmenu';
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { InputText } from "primereact/inputtext";
 import { Button } from 'primereact/button'
 
 import SelectRegion from './SelectRegion';
 
 import './Navbar.css';
+
+const items = [
+    { label: 'Home', icon: 'pi pi-fw pi-home', route: '/' },
+    { label: 'Admin', icon: 'pi pi-fw pi-database', route: '/admin' },
+    { label: 'Top Comps', icon: 'pi pi-fw pi-users', route: '/comps' },
+    { label: 'Leaderboards', icon: 'pi pi-fw pi-globe', route: '/leaderboards' },
+    { label: 'Login', icon: 'pi pi-fw pi-user', route: '/login' },
+    {
+        label: 'Search',
+        template: () => {
+            return (
+                <div style={{ marginTop: 5, marginLeft: 20 }}>
+                    <div className="p-inputgroup">
+                        <SelectRegion />
+                        <InputText placeholder="Search Summoner" type="text" />
+                        <Button icon="pi pi-search" className="p-button" />
+                    </div>
+                </div>
+            );
+        },
+    }
+];
+
 export default () => {
     const navigate = useNavigate();
-    const [activeIndex, setActiveIndex] = useState(0)
-
-    const items = [
-        { label: 'Home', icon: 'pi pi-fw pi-home', route: '/' },
-        { label: 'Admin', icon: 'pi pi-fw pi-database', route: '/admin' },
-        { label: 'Top Comps', icon: 'pi pi-fw pi-users', route: '/comps' },
-        { label: 'Leaderboards', icon: 'pi pi-fw pi-globe', route: '/leaderboards' },
-        { label: 'Guides', icon: 'pi pi-fw pi-book', route: '/guides' },
-        {
-            label: 'Search',
-            template: () => {
-                return (
-                    <div style={{ marginTop: 5, marginLeft: 20 }}>
-                        <div className="p-inputgroup">
-                            <SelectRegion />
-                            <InputText placeholder="Search Summoner" type="text" />
-                            <Button icon="pi pi-search" className="p-button" />
-                        </div>
-                    </div>
-                );
-            },
-        }
-    ];
+    const url = useLocation();
+    const [activeIndex, setActiveIndex] = useState(url.pathname === '/' ? 0 : items.findIndex(item => item.route === url.pathname));
 
     const onTabChangeHandler = (newTab) => {
         setActiveIndex(newTab.index);
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,104 +1,11 @@
-import React, { useState } from "react";
-
-import { useForm, Controller } from "react-hook-form";
-import { InputText } from "primereact/inputtext";
+import React from "react";
 import { Button } from "primereact/button";
-import { classNames } from "primereact/utils";
 
-import { createChampion } from "../api/champion";
 
 import "../components/AuthForm.css";
 
-export default ({ onLogout }) => {
-  const [formData, setFormData] = useState({});
-  const defaultValues = {
-    name: "",
-    cost: "",
-  };
-
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    reset
-  } = useForm({ defaultValues });
-
-  const onSubmit = (data) => {
-    setFormData(data);
-    createChampion(data);
-    reset();
-  };
-
-  const getFormErrorMessage = (name) =>
-    errors[name] && <small className="p-error">{errors[name].message}</small>;
-
+export default () => {
   return (
-    <>
-      <div className="form-demo p-col" style={{ marginTop: 50 }}>
-        <div className="p-d-flex p-jc-center">
-          <div className="card">
-            <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
-
-              {/* Name Field */}
-              <div className="p-field">
-                <span className="p-float-label p-input-icon-right">
-                  <i className="pi pi-user-edit" />
-                  <Controller
-                    name="name"
-                    control={control}
-                    rules={{ required: "Name is required." }}
-                    render={({ field, fieldState }) => (
-                      <InputText
-                        id={field.name}
-                        {...field}
-                        autoFocus
-                        className={classNames({
-                          "p-invalid": fieldState.invalid,
-                        })}
-                      />
-                    )}
-                  />
-                  <label htmlFor="name" className={classNames({ "p-error": errors.name })}>
-                    Champion Name
-                  </label>
-                </span>
-                {getFormErrorMessage("name")}
-              </div>
-
-              {/* Cost Field */}
-              <div className="p-field">
-                <span className="p-float-label p-input-icon-right">
-                  <i className="pi pi-dollar" />
-                  <Controller
-                    name="cost"
-                    control={control}
-                    rules={{
-                      required: "Cost is required.",
-                    }}
-                    render={({ field, fieldState }) => (
-                      <InputText
-                        id={field.name}
-                        {...field}
-                        className={classNames({
-                          "p-invalid": fieldState.invalid,
-                        })}
-                      />
-                    )}
-                  />
-                  <label htmlFor="cost" className={classNames({ "p-error": !!errors.cost })}>
-                    Cost
-                  </label>
-                </span>
-                {getFormErrorMessage("cost")}
-              </div>
-
-              {/* Submit Button */}
-              <Button type="submit" label="Create" className="p-mt-2" />
-            </form>
-          </div>
-        </div>
-      </div>
-      <Button label="Sign Out" onClick={() => onLogout()} />
-    </>
+    <Button label="Sign Out" onClick={() => localStorage.removeItem('token')} />
   );
 }
